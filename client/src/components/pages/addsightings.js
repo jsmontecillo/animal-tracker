a6a145f6c6c75b327c5a041c4cb5d241be16aeea
jsmontecillo <jsmontecillo@gmail.com>
2022-09-30 14:14:52 -0700

    added sort buttons for sightings

--- client/src/components/pages/addsightings.js
+++ client/src/components/pages/addsightings.js
@@ -7,6 +7,7 @@ import './addsighting.css';
 const SightingsForm = () => {
   const [sightings, setSightings] = useState([]);
   const [values, setValues] = useState({sighting_date: "", individual: "", location: "", appeared_healthy: false, record_date: "", individual_id: ""});
+  const locations = ['https://i.postimg.cc/3whGLXzQ/1257310.jpg', 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/dawn-at-dream-lake-rocky-mountain-national-park-square-format-gregory-ballos.jpg', 'https://i.postimg.cc/NjZtYbBZ/303127a88f88484489fbe783a265902c.jpg', 'https://i.postimg.cc/t4B5VmYm/8a7ddd5cee0f9972da7632dd7e2571ef.jpg', 'https://i.postimg.cc/kGwVhLr0/johnston-atoll-aerial.jpg', 'https://i.postimg.cc/T1hGfs5M/blue-ridge-parkway-nc-sunset-north-carolina-mountains-landscape-dave-allen.jpg', 'https://i.postimg.cc/bdX5WVQR/Laguna-Colorada-Bolivia.jpg', 'https://as2.ftcdn.net/v2/jpg/02/01/14/15/1000_F_201141509_PTNGIvWD8eeqhOD4MaDpbFe8P8AICo0S.jpg', 'https://img-aws.ehowcdn.com/560x560p/photos.demandstudios.com/getty/article/151/31/92849527.jpg', 'https://live.staticflickr.com/4467/36982436300_f6d021041b_c.jpg']
 
   const getSightings = async () => {
     const response = await fetch('http://localhost:7070/api/sightings');
@@ -47,6 +48,7 @@ console.log(values);
     console.log(sightings);
     setValues({sighting_date: "", individual: "", location: "", appeared_healthy: "", record_date: "", individual_id: ""});
   }
+
 
   const onDelete = async (ID) => {
     let response = await fetch(`http://localhost:7070/api/sightings/${ID}`, {method: "DELETE"})
@@ -143,12 +145,39 @@ console.log(values);
 
         <div className="sightings">
           <h3>Recent Sightings</h3>
+          <button type="button" onClick={() => {
+            let arr = [...sightings];
+            let sorted = arr.sort((a,b) => {
+            let d1 = new Date(a.sighting_date);
+            let d2 = new Date(b.sighting_date);
+            return d1.getMonth() - d2.getMonth()}); 
+            setSightings(sorted);
+          console.log(sightings)}
+            } style={{backgroundColor: "white", borderRadius: "6px", border: "solid 1px beige", fontFamily: 'Inter Tight', fontSize: "15px", padding: "5px", color: "#4E3524", fontWeight: "bold"}}>SORT BY DATE</button>
+          
+          <button type="button" onClick={() => {
+            let arr = [...sightings];
+            let sorted = arr.sort((a,b) => {
+              return  a.individual.localeCompare(b.individual)}); 
+            setSightings(sorted);
+          console.log(sightings)}
+            } style={{backgroundColor: "white", borderRadius: "6px", border: "solid 1px beige", fontFamily: 'Inter Tight', fontSize: "15px", padding: "5px", color: "#4E3524", fontWeight: "bold"}}>A-Z</button>
           <div className="container">
             {sightings.map((sight) => {
+              let d = new Date(sight.sighting_date);
+              const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'October', 'November', 'December']
               return (
                 <div key={sight.id} className="sighting-card">
-                  {sight.individual}: {sight.location}, {sight.sighting_date}, {sight.individual_id}. {sight.record_date}, Contact: {sight.email}
-                  <br/><button type="button" onClick={() => {onDelete(sight.id)}}>DELETE</button>
+                  <div className="sight-image">
+                    <img src={locations[sight.individual_id-1]} alt={sight.individual} height="300px" />
+                  </div>
+                  <ul style={{listStyle: "none"}}>
+                    <li style={{marginLeft: "-40px", marginTop: "-10px", fontWeight: "bold", fontSize: "20px"}}>{sight.individual}</li>
+                    <li style={{marginLeft: "-40px"}}>{sight.location}</li>
+                    <li style={{marginLeft: "-40px"}}>{months[d.getMonth()-1]}</li>
+                    <li style={{marginLeft: "-40px"}}>Contact: {sight.email}</li>
+                  </ul>
+                  <br/><button type="button" className="button-83" style={{marginLeft: "100px"}} onClick={() => {onDelete(sight.id)}}>DELETE</button>
                 </div>
               )
             })}
--- client/src/components/pages/layout.js
+++ client/src/components/pages/layout.js
@@ -7,7 +7,7 @@ import header from '../header.jpg';
 
 function Layout() {
   const handleDonate = () => {
-
+    alert('CONGRATS YOU WON AN IPHONE!!!!!!!!!!!')
   }
   return (
     <>
@@ -16,7 +16,7 @@ function Layout() {
         <Link to="/add-individuals" className="link">ADD INDIVIDUAL</Link>
         <Link to="/add-sighting" className="link">SUBMIT SIGHTINGS</Link>
         <span>
-          <button className="donate" onClick={handleDonate}>DONATE</button>
+          <button className="donate" onClick={handleDonate} style={{backgroundColor:"red", padding: "10px", borderRadius: "6px", border: "solid 1px white", color: "white"}}>DONATE</button>
         </span>
       </nav>
 
